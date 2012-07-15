33cfd9d628e70631f4ed6ba2d039ab762f0939e8
Adrian Gonzalez <bernardogzzf@gmail.com>
2012-07-15 14:27:11 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeochatRails::Application.config.secret_token = '5c129cac99bd863323277966556a7a003c5d8e11af4ee82c50bcf83e3386eadca9035498332bfea5469c1f45b7384f694bbb190e4e6a53c02892898bd6198465'
