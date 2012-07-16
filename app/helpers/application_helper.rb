6c46c157ed0466d0cc9f9061e275969c70e2e09e
Adrian Gonzalez <bernardogzzf@gmail.com>
2012-07-16 01:26:18 -0500

    se corrigo el envio de mensajes solo al canal que le toca

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
+  def suscribe(channel)
+    %Q(
+    $(document).ready(function(){
+      faye = new Faye.Client('http://localhost:5000/faye')
+      faye.subscribe("#{channel}", function(message){
+        eval(message)
+        scroll_to_bottom("#messages")
+      });
+    })
+    ).html_safe
+  end
+
 end
