0d9e7e5f59756f6f6830aa4704ad4d0749dd63d2
Adrian Gonzalez <bernardogzzf@gmail.com>
2012-08-07 12:57:00 -0500

    corrige el id del place al enviar el mensaje y el created_at

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 GeochatRails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  SOCKETIO_SERVER = "http://geochat-socketio.herokuapp.com/faye"
+  SOCKETIO_SERVER = "http://geochat-socketio.herokuapp.com/"
   FOURSQUARE_CLIENT_ID = "clientId"
   FOURSQUARE_CLIENT_SECRET = "clientSecret"
   # Code is not reloaded between requests
